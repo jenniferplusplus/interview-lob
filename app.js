3ac1b68a2e9268af121ea377b57b8247f154e87c
Jennifer Moore <jenniferplusplus@gmail.com>
2021-06-02 03:50:01 -0500

    removed scaffolded routes and added readme

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const addressRouter = require('./routes/address')
 
 var app = express();
@@ -16,8 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/address', addressRouter);
 
 module.exports = app;
